4acb61a4b2f7d3d0c4da1c39cb2b2044031c606f
Mats Pedersen <matsjp@stud.ntnu.no>
2019-01-28 13:55:09 +0100

    Added filter

--- app/src/main/java/com/example/nordichome/MainActivity.java
+++ app/src/main/java/com/example/nordichome/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.nordichome;
 
 
+import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import no.nordicsemi.android.support.v18.scanner.BluetoothLeScannerCompat;
+import no.nordicsemi.android.support.v18.scanner.ScanFilter;
 import no.nordicsemi.android.support.v18.scanner.ScanSettings;
 import no.nordicsemi.android.support.v18.scanner.ScanCallback;
 import no.nordicsemi.android.support.v18.scanner.ScanResult;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private UUID filterUuid = UUID.fromString("00001827-0000-1000-8000-00805F9B34FB");
 
     final ScanSettings settings = new ScanSettings.Builder()
             .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
@@ -29,9 +36,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
-        scanner.startScan(scanCallback);
 
+        final List<ScanFilter> filters = new ArrayList<>();
+        filters.add(new ScanFilter.Builder().setServiceUuid(new ParcelUuid((filterUuid))).build());
+        final BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
+        scanner.startScan(filters, settings, scanCallback);
     }
 
     private final ScanCallback scanCallback = new ScanCallback() {

f935b2643991f07e4efcf50328e141905b85f8cb
Mats Pedersen <matsjp@stud.ntnu.no>
2019-01-29 15:38:12 +0100

    Able to light up the LED

---
+++ app/src/main/java/com/example/nordichome/BleMeshManager.java
@@ -0,0 +1,268 @@
+package com.example.nordichome;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.UUID;
+
+import no.nordicsemi.android.ble.BleManager;
+import no.nordicsemi.android.ble.Request;
+import no.nordicsemi.android.log.LogContract;
+import no.nordicsemi.android.meshprovisioner.utils.MeshParserUtils;
+
+public class BleMeshManager extends BleManager<BleMeshManagerCallbacks> {
+
+    /**
+     * Mesh provisioning service UUID
+     */
+    public final static UUID MESH_PROVISIONING_UUID = UUID.fromString("00001827-0000-1000-8000-00805F9B34FB");
+    /**
+     * The maximum packet size is 20 bytes.
+     */
+    private static final int MAX_PACKET_SIZE = 20;
+    public static final int MTU_SIZE_MIN = 23;
+    private static final int MTU_SIZE_MAX = 517;
+    /**
+     * Mesh provisioning data in characteristic UUID
+     */
+    private final static UUID MESH_PROVISIONING_DATA_IN = UUID.fromString("00002ADB-0000-1000-8000-00805F9B34FB");
+    /**
+     * Mesh provisioning data out characteristic UUID
+     */
+    private final static UUID MESH_PROVISIONING_DATA_OUT = UUID.fromString("00002ADC-0000-1000-8000-00805F9B34FB");
+
+    /**
+     * Mesh provisioning service UUID
+     */
+    public final static UUID MESH_PROXY_UUID = UUID.fromString("00001828-0000-1000-8000-00805F9B34FB");
+
+    /**
+     * Mesh provisioning data in characteristic UUID
+     */
+    private final static UUID MESH_PROXY_DATA_IN = UUID.fromString("00002ADD-0000-1000-8000-00805F9B34FB");
+
+    /**
+     * Mesh provisioning data out characteristic UUID
+     */
+    private final static UUID MESH_PROXY_DATA_OUT = UUID.fromString("00002ADE-0000-1000-8000-00805F9B34FB");
+
+    private final String TAG = BleMeshManager.class.getSimpleName();
+    private BluetoothGattCharacteristic mMeshProvisioningDataInCharacteristic, mMeshProvisioningDataOutCharacteristic;
+    private BluetoothGattCharacteristic mMeshProxyDataInCharacteristic;
+    private BluetoothGattCharacteristic mMeshProxyDataOutCharacteristic;
+
+    private int mtuSize = MAX_PACKET_SIZE;
+
+    private BluetoothGatt mBluetoothGatt;
+    private boolean isProvisioningComplete;
+    private boolean mIsDeviceReady;
+    /**
+     * BluetoothGatt callbacks for connection/disconnection, service discovery, receiving indication, etc
+     */
+    private final BleManagerGattCallback mGattCallback = new BleManagerGattCallback() {
+
+        @Override
+        protected Deque<Request> initGatt(final BluetoothGatt gatt) {
+            mBluetoothGatt = gatt;
+            final LinkedList<Request> requests = new LinkedList<>();
+            requests.add(Request.newMtuRequest(MTU_SIZE_MAX));
+            if (isProvisioningComplete) {
+                requests.add(Request.newReadRequest(mMeshProxyDataOutCharacteristic));
+                requests.add(Request.newReadRequest(mMeshProxyDataInCharacteristic));
+                requests.add(Request.newEnableNotificationsRequest(mMeshProxyDataOutCharacteristic));
+            } else {
+                requests.add(Request.newReadRequest(mMeshProvisioningDataInCharacteristic));
+                requests.add(Request.newReadRequest(mMeshProvisioningDataOutCharacteristic));
+                requests.add(Request.newEnableNotificationsRequest(mMeshProvisioningDataOutCharacteristic));
+            }
+            return requests;
+        }
+
+        @Override
+        public boolean isRequiredServiceSupported(final BluetoothGatt gatt) {
+            boolean writeRequest;
+
+            BluetoothGattService meshService = gatt.getService(MESH_PROXY_UUID);
+            if (meshService != null) {
+                isProvisioningComplete = true;
+                mMeshProxyDataInCharacteristic = meshService.getCharacteristic(MESH_PROXY_DATA_IN);
+                mMeshProxyDataOutCharacteristic = meshService.getCharacteristic(MESH_PROXY_DATA_OUT);
+
+                writeRequest = false;
+                if (mMeshProxyDataInCharacteristic != null) {
+                    final int rxProperties = mMeshProxyDataInCharacteristic.getProperties();
+                    writeRequest = (rxProperties & BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE) > 0;
+                }
+                return mMeshProxyDataInCharacteristic != null && mMeshProxyDataOutCharacteristic != null && writeRequest;
+            } else {
+                meshService = gatt.getService(MESH_PROVISIONING_UUID);
+                if (meshService != null) {
+                    isProvisioningComplete = false;
+                    mMeshProvisioningDataInCharacteristic = meshService.getCharacteristic(MESH_PROVISIONING_DATA_IN);
+                    mMeshProvisioningDataOutCharacteristic = meshService.getCharacteristic(MESH_PROVISIONING_DATA_OUT);
+
+                    writeRequest = false;
+                    if (mMeshProvisioningDataInCharacteristic != null) {
+                        final int rxProperties = mMeshProvisioningDataInCharacteristic.getProperties();
+                        writeRequest = (rxProperties & BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE) > 0;
+                    }
+                    return mMeshProvisioningDataInCharacteristic != null && mMeshProvisioningDataInCharacteristic != null && writeRequest;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        protected void onDeviceDisconnected() {
+            mIsDeviceReady = false;
+            isProvisioningComplete = false;
+            mMeshProvisioningDataInCharacteristic = null;
+            mMeshProvisioningDataOutCharacteristic = null;
+            mMeshProxyDataInCharacteristic = null;
+            mMeshProxyDataOutCharacteristic = null;
+        }
+
+        @Override
+        protected void onCharacteristicRead(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
+        }
+
+        @Override
+        public void onCharacteristicWrite(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
+            final byte[] data = characteristic.getValue();
+            Log.v(TAG, "Data written: " + MeshParserUtils.bytesToHex(data, true));
+            mCallbacks.onDataSent(gatt.getDevice(), mtuSize, data);
+        }
+
+        @Override
+        public void onCharacteristicNotified(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
+            final byte[] data = characteristic.getValue();
+            Log.v(TAG, "Characteristic notified: " + MeshParserUtils.bytesToHex(data, true));
+            mCallbacks.onDataReceived(gatt.getDevice(), mtuSize, data);
+        }
+
+        @Override
+        protected void onMtuChanged(final int mtu) {
+            super.onMtuChanged(mtu);
+            mtuSize = mtu - 3;
+        }
+
+        @Override
+        protected void onDeviceReady() {
+            mIsDeviceReady = true;
+            super.onDeviceReady();
+        }
+    };
+
+    public BleMeshManager(final Context context) {
+        super(context);
+    }
+
+    @NonNull
+    @Override
+    protected BleManagerGattCallback getGattCallback() {
+        return mGattCallback;
+    }
+
+    public BluetoothDevice getBluetoothDevice(){
+        return mBluetoothDevice;
+    }
+
+    @Override
+    protected boolean shouldAutoConnect() {
+        // If you want to connect to the device using autoConnect flag = true, return true here.
+        // Read the documentation of this method.
+        return super.shouldAutoConnect();
+    }
+
+    /**
+     * Sends the mesh pdu
+     * <p>
+     *     The function will chunk the pdu to fit in to the mtu size supported by the node
+     * </p>
+     * @param pdu mesh pdu
+     */
+    public void sendPdu(final byte[] pdu) {
+        final int chunks = (pdu.length + (mtuSize - 1)) / mtuSize;
+        int srcOffset = 0;
+        if(chunks > 1) {
+            for (int i = 0; i < chunks; i++) {
+                final int length = Math.min(pdu.length - srcOffset, mtuSize);
+                final byte[] segmentedBuffer = new byte[length];
+                System.arraycopy(pdu, srcOffset, segmentedBuffer, 0, length);
+                srcOffset += length;
+                send(segmentedBuffer);
+            }
+        } else {
+            send(pdu);
+        }
+    }
+
+    /**
+     * Refreshes the device cache. This is to make sure that Android will discover the services as the the mesh node will change the provisioning service to a proxy service.
+     */
+    public boolean refreshDeviceCache(){
+        //Once the service discovery is complete we will refresh the device cache and discover the services again.
+        return refreshDeviceCache(mBluetoothGatt);
+    }
+
+    public boolean isProvisioningComplete() {
+        return isProvisioningComplete;
+    }
+
+    public void setProvisioningComplete(final boolean provisioningComplete) {
+        this.isProvisioningComplete = provisioningComplete;
+    }
+
+    public final int getMtuSize() {
+        return mtuSize;
+    }
+
+    private void send(final byte[] data) {
+        Log.v(TAG, "Sending data : " + MeshParserUtils.bytesToHex(data, true));
+        if (isProvisioningComplete) {
+            // Are we connected?
+            if (mMeshProxyDataInCharacteristic == null)
+                return;
+            final BluetoothGattCharacteristic characteristic = mMeshProxyDataInCharacteristic;
+            characteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
+            writeCharacteristic(characteristic, data);
+
+        } else {
+            // Are we connected?
+            if (mMeshProvisioningDataInCharacteristic == null)
+                return;
+            final BluetoothGattCharacteristic characteristic = mMeshProvisioningDataInCharacteristic;
+            characteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
+            writeCharacteristic(characteristic, data);
+        }
+    }
+
+    private boolean refreshDeviceCache(final BluetoothGatt gatt) {
+        if (gatt == null)
+            return false;
+        /*
+         * There is a refresh() method in BluetoothGatt class but for now it's hidden. We will call it using reflections.
+         */
+        try {
+            final Method refresh = gatt.getClass().getMethod("refresh");
+            if (refresh != null) {
+                return (Boolean) refresh.invoke(gatt);
+            }
+        } catch (final Exception e) {
+            log(LogContract.Log.Level.ERROR, "An exception occurred while refreshing device " + e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean isDeviceReady() {
+        return mIsDeviceReady;
+    }
+}
--- app/src/main/java/com/example/nordichome/MainActivity.java
+++ app/src/main/java/com/example/nordichome/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.nordichome;
 
 
 import android.os.ParcelUuid;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import no.nordicsemi.android.meshprovisioner.provisionerstates.UnprovisionedMeshNode;
 import no.nordicsemi.android.support.v18.scanner.BluetoothLeScannerCompat;
 import no.nordicsemi.android.support.v18.scanner.ScanFilter;
+import no.nordicsemi.android.support.v18.scanner.ScanRecord;
 import no.nordicsemi.android.support.v18.scanner.ScanSettings;
 import no.nordicsemi.android.support.v18.scanner.ScanCallback;
 import no.nordicsemi.android.support.v18.scanner.ScanResult;
@@ -21,6 +27,7 @@ import no.nordicsemi.android.support.v18.scanner.ScanResult;
 public class MainActivity extends AppCompatActivity {
 
     private UUID filterUuid = UUID.fromString("00001827-0000-1000-8000-00805F9B34FB");
+    private MeshRepo meshRepo;
 
     final ScanSettings settings = new ScanSettings.Builder()
             .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
@@ -31,26 +38,42 @@ public class MainActivity extends AppCompatActivity {
             // Samsung S6 and S6 Edge report equal value of RSSI for all devices. In this app we ignore the RSSI.
             /*.setUseHardwareBatchingIfSupported(false)*/
             .build();
-
+    private BluetoothLeScannerCompat scanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.scanButton);
+        meshRepo = new MeshRepo(this);
 
         final List<ScanFilter> filters = new ArrayList<>();
         filters.add(new ScanFilter.Builder().setServiceUuid(new ParcelUuid((filterUuid))).build());
-        final BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
-        scanner.startScan(filters, settings, scanCallback);
+        scanner = BluetoothLeScannerCompat.getScanner();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanner.startScan(filters, settings, scanCallback);
+            }
+        });
     }
 
     private final ScanCallback scanCallback = new ScanCallback() {
         @Override
         public void onScanResult(final int callbackType, final ScanResult result){
+            scanner.stopScan(this);
             Log.d("Scan", "Scan result");
             String address = result.getDevice().getAddress();
             Log.d("Address", address);
-
-
+            final byte[] serviceData = getServiceData(result, UUID.fromString("00001827-0000-1000-8000-00805F9B34FB"));
+            if (serviceData != null){
+                Log.d("ServiceData", "ServiceData");
+                final UUID uuid = meshRepo.getMeshManagerApi().getDeviceUuid(serviceData);
+                if (!meshRepo.getBleMeshManager().isConnected()){
+                    meshRepo.connect(result.getDevice());
+                }
+                //meshRepo.getMeshManagerApi().identifyNode(uuid, result.getDevice().getName());
+                //meshRepo.getMeshManagerApi().startProvisioning(meshRepo.getUnprovisionedMeshNode());
+            }
         }
 
         @Override
@@ -63,4 +86,13 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Scan", "Scan failed");
         }
     };
+
+    @Nullable
+    public static byte[] getServiceData(@NonNull final ScanResult result, @NonNull final UUID serviceUuid) {
+        final ScanRecord scanRecord = result.getScanRecord();
+        if (scanRecord != null) {
+            return scanRecord.getServiceData(new ParcelUuid((serviceUuid)));
+        }
+        return null;
+    }
 }

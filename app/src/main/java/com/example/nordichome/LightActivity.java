a1a519f8da9857d61ee7b7214538b7ebfe50e445
Heidibr <heidilb@stud.ntnu.no>
2019-02-21 12:50:26 +0100

    in progress #9

--- app/src/main/java/com/example/nordichome/LightActivity.java
+++ app/src/main/java/com/example/nordichome/LightActivity.java
@@ -1,13 +1,25 @@
 package com.example.nordichome;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import no.nordicsemi.android.meshprovisioner.transport.MeshModel;
+
 public class LightActivity extends AppCompatActivity {
+
+    private static final String TAG = LightActivity.class.getSimpleName();
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
+
     }
+
+
+
 }
--- app/src/main/java/com/example/nordichome/ScannerActivity.java
+++ app/src/main/java/com/example/nordichome/ScannerActivity.java
@@ -1,16 +1,12 @@
 package com.example.nordichome;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Button;
-
+import android.util.Log;
 
 import com.example.nordichome.adapter.DevicesAdapter;
 import com.example.nordichome.adapter.DiscoveredBluetoothDevice;
@@ -20,10 +16,6 @@ import viewmodels.ScannerRepo;
 public class ScannerActivity extends AppCompatActivity implements DevicesAdapter.OnItemClickListener {
 
     private ScannerRepo scannerRepo;
-
-    private CoordinatorLayout coordinatorLayout;
-    private Button button;
-
     private String TAG = ScannerActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,20 +33,6 @@ public class ScannerActivity extends AppCompatActivity implements DevicesAdapter
         final DevicesAdapter adapter = new DevicesAdapter(this, scannerRepo.getUnprovisionedDevicesLiveData());
         adapter.setOnItemClickListener(this);
         recyclerView.setAdapter(adapter);
-
-        coordinatorLayout = findViewById(R.id.myCoordinatorLayout);
-        button = findViewById(R.id.button);
-
-        button.setOnClickListener(v -> showSnackbar());
-    }
-
-    //Popup for connection to a device. Does not work correctly cant show connected message and then change page.
-    public void showSnackbar() {
-        Snackbar snackbar = Snackbar.make(coordinatorLayout, "Connected", Snackbar.LENGTH_SHORT);
-        snackbar.show();
-        //Starts new activity, LightActivity.
-        //final Intent intent = new Intent(ScannerActivity.this, LightActivity.class);
-        //startActivity(intent);
     }
 
     @Override
